a38502c80be943d8bbebc03f4ce108a87009a2b2
ulydev <ulydev@live.fr>
2016-08-22 15:31:33 +0200

    delay

--- server/main.js
+++ server/main.js
@@ -1,5 +1,25 @@
 import { Meteor } from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+SyncedCron.add({
+  name: 'Remove old Entries (16 Days)',
+  schedule: function(parser) {
+  	return parser.text('every 8 hours');
+  },
+  job: function() {
+  	var today = new Date();
+  	var targetDate = new Date();
+
+  	targetDate.setDate(today.getDate() - 16);
+  	targetDate.setHours(0);
+  	targetDate.setMinutes(0);
+  	targetDate.setSeconds(0);
+
+    var cursor = Uploads.find({createdAt: {$lt: targetDate}});
+    cursor.forEach(function(doc){
+      exec('rm -rf /.uploads/games/' + doc.id);
+    });
+
+  	// Remove matchng Documents
+  	Uploads.remove({createdAt: {$lt: targetDate}});
+  }
 });
